1729e2a8e2f7a0ad98e13d572d56700c76557d83
Kirubel141797 <kirubelteshome22@gmail.com>
2024-02-20 11:13:00 -0800

    lazana

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-// Displays the status code of a GET request
-
 const request = require('request');
-const argv = process.argv;
-let url = argv[2];
-
-request.get(url).on('response', function (response) {
+request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
